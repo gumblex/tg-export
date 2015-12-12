9d70b5c3dcef2e6536863c8db39426d8d0f37ac8
Dingyuan Wang <abcdoyle888@gmail.com>
2015-12-12 12:18:18 +0800

    add tgcli.py for general processing

---
+++ tgcli.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import json
+import socket
+import shutil
+import logging
+import tempfile
+import threading
+import subprocess
+
+tg_server_pub = '''-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6
+lyDONS789sVoD/xCS9Y0hkkC3gtL1tSfTlgCMOOul9lcixlEKzwKENj1Yz/s7daS
+an9tqw3bfUV/nqgbhGX81v/+7RFAEd+RwFnK7a+XYl9sluzHRyVVaTTveB2GazTw
+Efzk2DWgkBluml8OREmvfraX3bkHZJTKX4EQSjBbbdJ2ZXIsRrYOXfaA+xayEGB+
+8hdlLmAjbCVfaigxX0CDqWeR1yFL9kwd9P0NsZRPsmoqVwMbMu7mStFai6aIhc3n
+Slv8kg9qv1m6XHVQY3PnEw+QQtqSIXklHwIDAQAB
+-----END RSA PUBLIC KEY-----
+'''
+
+class TelegramCliInterface:
+    def __init__(self, cmd, run=True):
+        self.cmd = cmd
+        self.proc = None
+        self.sock = None
+        self.ready = threading.Event()
+        self.closed = False
+        self.thread = None
+        self.tmpdir = tempfile.mkdtemp()
+        # event callbacks
+        self.on_info = logging.info
+        self.on_json = logging.debug
+        self.on_start = lambda: logging.info('Telegram-cli started.')
+        self.on_exit = lambda: logging.warning('Telegram-cli died.')
+        if run:
+            self.run()
+
+    def _get_pubkey(self):
+        tgdir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(self.cmd)), '..'))
+        paths = [
+            os.path.join(tgdir, 'tg-server.pub'),
+            os.path.join(tgdir, 'server.pub'),
+            '/etc/telegram-cli/server.pub',
+            '/usr/local/etc/telegram-cli/server.pub',
+            os.path.join(self.tmpdir, 'tg-server.pub')
+        ]
+        for path in paths:
+            if os.path.isfile(path):
+                return path
+        else:
+            with open(path, 'w') as f:
+                f.write(tg_server_pub)
+            return path
+
+    def checkproc(self):
+        if self.proc and self.proc.poll() is None:
+            return self.proc
+        sockfile = os.path.join(self.tmpdir, 'tgcli.sock')
+        self.proc = subprocess.Popen((self.cmd, '-k', self._get_pubkey(), '--json', '-R', '-C', '-S', sockfile), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        while not os.path.exists(sockfile):
+            time.sleep(0.5)
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self.sock.connect(sockfile)
+        return self.proc
+
+    def _run_cli(self):
+        while not self.closed:
+            self.checkproc()
+            self.ready.set()
+            self.on_start()
+            try:
+                while self.ready.is_set():
+                    out = self.proc.stdout.readline().decode('utf-8')
+                    if out:
+                        if out[0] in '[{':
+                            self.on_json(out.strip())
+                        else:
+                            self.on_info(out.strip())
+                    else:
+                        break
+            except BrokenPipeError:
+                pass
+            finally:
+                self.proc.terminate()
+            self.ready.clear()
+            self.on_exit()
+
+    def run(self):
+        self.thread = threading.Thread(target=self._run_cli)
+        self.thread.daemon = True
+        self.thread.start()
+
+    def close(self):
+        self.ready.clear()
+        self.closed = True
+        if self.thread:
+            self.thread.join(2)
+        if self.tmpdir:
+            shutil.rmtree(self.tmpdir, True)
+
+    def __enter__(self):
+        if not self.thread:
+            self.run()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def __del__(self):
+        self.close()
+
+    def send_command(self, cmd, timeout=180, resync=True):
+        self.checkproc()
+        self.sock.settimeout(timeout)
+        self.sock.sendall(cmd.encode('utf-8') + b'\n')
+        data = self.sock.recv(1024)
+        lines = data.split(b'\n', 1)
+        if not lines[0].startswith(b'ANSWER '):
+            if resync:
+                while not lines[0].startswith(b'ANSWER '):
+                    data = self.sock.recv(1024)
+                    lines = data.split(b'\n', 1)
+            else:
+                raise ValueError('Bad reply from telegram-cli: %s' % lines[0][:20])
+        size = int(lines[0][7:].decode('ascii'))
+        reply = lines[1] if len(lines) == 2 else b''
+        while len(reply) < size:
+            reply += self.sock.recv(1024)
+        ret = reply.decode('utf-8')
+        try:
+            return json.loads(ret)
+        except ValueError:
+            return ret
+
+    def __getattr__(self, name):
+        if name.startswith('cmd_'):
+            fn = lambda *args, **kwargs: self.send_command(' '.join(map(str, (name[4:],) + args)), **kwargs)
+            return fn
+        else:
+            raise AttributeError
+
+if __name__ == "__main__":
+    import sys
+    logging.basicConfig(stream=sys.stderr, format='%(asctime)s [%(levelname)s] %(message)s', level=logging.DEBUG)
+    tgcli = TelegramCliInterface(sys.argv[1])
+    for ln in sys.stdin:
+        print(tgcli.send_command(ln.strip()))
