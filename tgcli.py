5a00b016fbf8dce15232c6347a2c1182792bd178
Dingyuan Wang <abcdoyle888@gmail.com>
2016-03-12 15:51:12 +0800

    add license header

--- tgcli.py
+++ tgcli.py
@@ -12,6 +12,24 @@ import tempfile
 import threading
 import subprocess
 
+'''
+tgcli.py - Library to interact with telegram-cli.
+Copyright (C) 2015-2016  Dingyuan Wang
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 tg_server_pub = '''-----BEGIN RSA PUBLIC KEY-----
 MIIBCgKCAQEAwVACPi9w23mF3tBkdZz+zwrzKOaaQdr01vAbU4E1pvkfj4sqDsm6
 lyDONS789sVoD/xCS9Y0hkkC3gtL1tSfTlgCMOOul9lcixlEKzwKENj1Yz/s7daS
